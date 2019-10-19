852f8ea558f0203be548cac6c7c4354500991555
Rahul_Aggarwal <rahulaggarwal1016@gmail.com>
2019-10-19 14:18:21 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');     // Import routes module we created
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Define port to listen to
+const PORT = process.env.PORT || 8082;
+
+// allows us to parse bodies of requests
+app.use(bodyParser.json());
+// allows for rich objects and arrays to be encoded into URL-encoded format
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow cross origin resource sharing
+app.use(cors())
+
+// Connect all routes from routes module to app
+app.use('/', routes);
+
+// Start Server
+app.listen(PORT, () => console.log(`Server is running on ${PORT}!`));
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const UsersRouter = require('express').Router();
+const usersDB = require('../db/users');
+
+// Define routes
+UsersRouter.get('/', (req, res) => res.send('Welcome to Road to Node!'));
+
+// Let's try to implement these endpoints
+// HINT: Look at ./db/users.js for database functions you can use!
+
+// Get a list of users  (GET)
+// http://localhost:8080/users/list
+UsersRouter.get("/list", async (req, res) => {
+  const list = await usersDB.getAllUsers();
+  res.send(list);
+});
+
+// Create new user (POST)
+// http://localhost:8080/users/create
+// Uses 'first', 'last', 'id' in req.body
+// const user = { first, last, id };
+
+UsersRouter.post("/create", async (req, res) => {
+  const list = await usersDB.setUser();
+  res.send(user);
+});
+
+
+// Gets user by id (GET)
+// http://localhost:8080/users/query/peterparker
+// /:id
+// Use req.params.id to retrieve id
+
+
+// Deletes user by id (DELETE)
+// http://localhost:8080/users/delete/peterparker
+
+
+// Export router
+module.exports = UsersRouter;
